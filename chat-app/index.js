3224fd8638ade379d7d40a6ea9102979f573d8d9
Saif Al Mahmud <saifalmahmud@u.boisestate.edu>
2017-12-06 17:20:39 -0700

    Final Commit for chat

--- chat-app/index.js
+++ chat-app/index.js
@@ -17,16 +17,7 @@ app.get('/', function(req, res) {
 
 ////////// REST ////////////
 // API Provider
-app.get('/api/showuser', function(req, res) {
-    res.json({status: 200, users: [{name: "nuhil", age: "30"}, {name: "prottoy", age: "26"}]});
-});
 
-app.post('/api/adduser', function(req, res) {
-    console.log('I am ready to add data');
-    // To Do:
-    // add those data into database
-    res.json({status: 201, message: "Data added successfully!"});
-});
 ///////////////
 
 const server = app.listen(app.get('port'), function() {
@@ -37,20 +28,6 @@ const io = socket(server);
 
 io.on('connection', function(socket) {
     console.log('Some one is just connected..');
-    // To Do:
-    // Add users info into DB
-    // console.log(socket);
-
-    // var Schema = mongoose.Schema,
-    // ObjectId = Schema.ObjectId;
- 
-    // var User = new Schema({
-    //     user_id    : ObjectId,
-    //     browser     : String,
-    //     date      : Date
-    // });
-
-
 
     socket.on('chat', function(data) {
         console.log(data);
